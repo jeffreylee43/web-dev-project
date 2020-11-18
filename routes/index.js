ed74fb9bb7783f696175d473d3624b7b45c4656a
Jeffrey Lee <jlee52@stevens.edu>
2020-11-18 00:26:58 -0500

    change in route format for login/register

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const usersRoutes = require("./users");
 const stocksRoutes = require("./stocks");
 const companiesRoutes = require("./companies");
+const loginRoutes = require("./login");
+const registerRoutes = require("./register");
 
 
 const constructorMethod = (app) => {
@@ -8,10 +10,11 @@ const constructorMethod = (app) => {
     res.render('landing/landingpage', {title: "Home"});
   });
 
+  app.use('/login', loginRoutes);
+  app.use('/register', registerRoutes);
   app.use('/users', usersRoutes);
   app.use('/stocks', stocksRoutes);
   app.use('/companies', companiesRoutes);
-
 
   app.use('*', (req, res) => {
     res.status(404).json({ error: 'Provided route is not found' });
--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/login', async (req, res) => {
-    res.render('users/login', { title: "Login"});
-});
-
-router.get('/register', async (req, res) => {
-    res.render('users/register', {title: "Register"});
-});
-
 router.get('/dashboard', async (req, res) => {
     res.render('users/dashboard', {title: "Your Dashboard"});
 });
