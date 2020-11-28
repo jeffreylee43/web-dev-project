50f73cf0447c3d06da6a682c889f0923df8afa90
spatel49 <sypatel18@gmail.com>
2020-11-27 22:38:37 -0500

    Finished History Page. Connected traders to reviews on database

--- routes/history.js
+++ routes/history.js
@@ -3,19 +3,20 @@ const router = express.Router();
 const data = require('../data');
 const companies = data.companies;
 const traders = data.traders;
+const reviews = data.reviews;
 
-router.get('/stocksList', async (req, res) => {
+router.get('/', async (req, res) => {
     if(!req.session.user) {
         return res.status(403).render('users/notLoggedIn', {title: "Not Logged In", loggedIn: false});
     }
     try {
-        let actionItem = "" + new Date() + ": Viewed the Stocks List.";
+        let actionItem = "" + new Date() + ": Viewed User History.";
         const updateHistory = await traders.addTraderHistory(req.session.user._id, actionItem);
-        const allCompanies = await companies.getAllCompanies();
-        res.render('stocks/stocksList', {
-            title: 'List of Stocks',
+        const userInfo = await traders.getTraderById(req.session.user._id);
+        res.render('users/history', {
+            title: 'User History',
             loggedIn: true,
-            allCompanies: allCompanies,
+            userInfo: userInfo
         });
     } catch (e) {
         res.status(500).json({ error: e });
