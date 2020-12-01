b02f11089914acf601e4d4960acd239adf4ddc42
Jeffrey Lee <jlee52@stevens.edu>
2020-12-01 00:58:18 -0500

    navbar, logout route fix, add button fix

--- routes/stocks.js
+++ routes/stocks.js
@@ -12,10 +12,19 @@ router.get('/stocksList', async (req, res) => {
         let actionItem = "" + new Date() + ": Viewed the Stocks List.";
         const updateHistory = await traders.addTraderHistory(req.session.user._id, actionItem);
         const allCompanies = await companies.getAllCompanies();
+
+        // Updating prices of stocks when loading stocks list page (issue: eventually gets an error, i believe it's b/c of api calls)
+        // for(let company of allCompanies) {
+        //     await companies.updateCompany(company.ticker);
+        // }
+
+        // const allCompaniesUpdated = await companies.getAllCompanies();
+
         res.render('stocks/stocksList', {
             title: 'List of Stocks',
             loggedIn: true,
-            allCompanies: allCompanies,
+            allCompanies: allCompanies
+            // allCompanies: allCompaniesUpdated
         });
     } catch (e) {
         res.status(500).json({ error: e });
