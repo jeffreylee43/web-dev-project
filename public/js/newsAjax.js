0322e4b4611a2eeee0f7a088f6381b642ac8a3fe
spatel49 <sypatel18@gmail.com>
2020-12-16 17:25:55 -0500

    ajax small stuff

--- public/js/newsAjax.js
+++ public/js/newsAjax.js
@@ -13,20 +13,25 @@ $(document).ready(() => {
                 $.each(data.articles, function(index, newsData){
                     let article = `<div class = "news-div">
                     <img src="${newsData.urlToImage}" alt="${newsData.source.name}" class="news-img"><br>
-                <a href="${newsData.url}" target="_blank" class="news-title">${newsData.title}</a>
-                <p class="news-desc">${newsData.description}</p>
-                <p class="news-date">Date Published: ${newsData.publishedAt}</p>
-                </div>`
+                    <a href="${newsData.url}" target="_blank" class="news-title">${newsData.title}</a>
+                    <p class="news-desc">${newsData.description}</p>
+                    <p class="news-date">Date Published: ${newsData.publishedAt}</p>
+                    </div>`
                 $(".top-news-div").append(article);
                 })
                 $('#myHeader').empty();
                 let header = `Results for: ${$('#newsTerm').val()} `
                 $('#myHeader').append(header)
                 if(data.articles.length == 0){
-                    let error = `<p> No news found for ${$('#newsTerm').val()}  within the past day`
+                    $(".top-news-div").empty();
+                    let error = `<p> No news found for '${$('#newsTerm').val()}'  within the past day`
                     $(".top-news-div").append(error);
                 }
             })
+        } else {
+            $(".top-news-div").empty();
+            let error = `<p> Not a valid search term '${$('#newsTerm').val()}'</p> <p>Please try again.</p>`
+            $(".top-news-div").append(error);
         }
     })
 })
